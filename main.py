c187666f7271784ed3e9a5f99d7f9f30e02ad02c
rbochukova <76839501+rbochukova@users.noreply.github.com>
2025-12-14 15:13:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from modelling import predict_test, train_model
 from resnet_model import BirdResNet34
 
 if __name__ == "__main__":
-    train_model(num_epochs=NUM_EPOCHS, batch_size=BATCH_SIZE)
-    predict_test(BirdResNet34, batch_size=BATCH_SIZE)
+    train_model(num_epochs=NUM_EPOCHS, batch_size=BATCH_SIZE, dropout_p=0.2)
+    predict_test(batch_size=BATCH_SIZE)
